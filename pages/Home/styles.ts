4819a73382ebec1ca20a00b38adefaf5dafd54b3
João Augusto <joao.augusto@foodzilla.com.br>
2021-11-21 23:24:36 -0300

    first commit

---
+++ pages/Home/styles.ts
@@ -0,0 +1,178 @@
+import styled, { css } from "styled-components";
+import Link from "next/link";
+
+export const Container = styled.div`
+  background: linear-gradient(
+    158.86deg,
+    #1c2a34 10.36%,
+    #111a20 49.58%,
+    #111a20 49.58%
+  );
+  width: 100vw;
+  min-height: 100vh;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: center;
+
+  padding-top: 20px;
+  width: 100%;
+
+  > div {
+    width: 50%;
+
+    display: flex;
+    justify-content: space-around;
+
+    @media (max-width: 800px) {
+      width: 100%;
+    }
+  }
+`;
+
+export const ButtonHeader = styled.div`
+  font-family: "Montserrat", sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 17px;
+
+  text-align: center;
+
+  color: #ffffff;
+
+  cursor: pointer;
+
+  ${({ active }) =>
+    active &&
+    css`
+      border-bottom: 2px solid #fff;
+    `}
+`;
+
+export const Image = styled.img`
+  width: 470.97px;
+  height: 470.97px;
+
+  object-fit: contain;
+
+  position: absolute;
+  top: 100px;
+
+  @media (max-width: 488px) {
+    width: 70%;
+    top: -120px;
+    left: -40px;
+  }
+`;
+
+export const Title = styled.p`
+  font-family: "Montserrat", sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 74.0596px;
+  line-height: 90px;
+  text-align: center;
+
+  margin: 10px;
+  color: #ffffff;
+
+  padding: 10px;
+
+  @media (max-width: 517px) {
+    font-size: 50px;
+  }
+`;
+
+export const Paragraph = styled.p`
+  font-family: "Montserrat", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 30px;
+  text-align: center;
+
+  color: #ffffff;
+
+  padding: 10px;
+
+  @media (max-width: 800px) {
+    font-size: 17px;
+
+    > br {
+      display: none;
+    }
+  }
+`;
+
+export const ButtonGreen = styled.div`
+  width: 130.76px;
+  height: 56.7px;
+
+  background: #b0fbbc;
+  border-radius: 8px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-family: "Montserrat", sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18.5149px;
+  line-height: 23px;
+  text-align: center;
+
+  cursor: pointer;
+
+  margin-right: 20px;
+
+  @media only screen and (max-width: 378px) {
+    margin-bottom: 20px;
+  }
+`;
+
+export const ButtonGray = styled.div`
+  width: 229.12px;
+  height: 56.7px;
+
+  background: #1c2c35;
+  border-radius: 8px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-family: "Montserrat", sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18.5149px;
+  line-height: 23px;
+  text-align: center;
+
+  cursor: pointer;
+
+  color: #ffffff;
+`;
+
+export const Footer = styled.div``;
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+
+  position: absolute;
+
+  flex-direction: column;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+  flex-wrap: wrap;
+`;
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import Home from "./Home";
+
+const Index = () => <Home />;
+
+export default Index;
