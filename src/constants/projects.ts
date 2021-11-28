99f43387c4b1d368ded31003b44a83350f4f97b5
João Augusto <joao.augusto@foodzilla.com.br>
2021-11-28 20:36:12 -0300

    feat: adding more posts and fixing posts styles

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,1 +1,13 @@
-export const projects = [] as Array<{ title: string; description: string; gitLink: string }>
+export const projects = [
+  {
+    title: 'Express Http Redis',
+    description:
+      'Biblioteca para auxiliar no cacheamento de informações com redis, através de middlewares que auxiliam no processo e diminuem o tempo das requisições',
+    gitLink: 'https://github.com/Joao208/express-http-redis',
+  },
+  {
+    title: 'Errors stack',
+    description: 'Error Class é uma biblioteca para ajudar a lançar novos erros com nomes, tipos e mensagens personalizadas',
+    gitLink: 'https://github.com/Joao208/errors-stack',
+  },
+] as Array<{ title: string; description: string; gitLink: string }>
