5f18daa824fc9e59293ec6615e3d8f3246500eae
João Augusto <joao.augusto@foodzilla.com.br>
2021-11-28 22:21:04 -0300

    fix: orthography fixing

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Twitter V2',
     description:
-      'Biblioteca Para a versão 2 da api do twitter, para auxiliar no uso dos métodos da api, com typescript, e nodejs',
+      'Biblioteca para a segunda versão da API do Twitter, que irá auxiliar o uso dos métodos da API, com Typescript, e NodeJS.',
     gitLink: 'https://github.com/Joao208/v2-twitter',
   },
 ] as Array<{ title: string; description: string; gitLink: string }>
