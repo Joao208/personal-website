280095a98c0b45d1509832eaba752d64a9794796
João Augusto <joao.augusto@foodzilla.com.br>
2021-12-05 13:24:49 -0300

    feat: adding manifest

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta name="theme-color" content="#1C2A34" media="(prefers-color-scheme: dark)" />
+          <link rel="manifest" href="manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
