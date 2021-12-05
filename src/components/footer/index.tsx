b6341d4249f80d3bc46cebe664f6f172f8becc11
João Augusto <joao.augusto@foodzilla.com.br>
2021-12-05 20:07:39 -0300

    feat: small fix in footer and buttons

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,29 +3,50 @@ import { newSubscription } from 'src/services'
 import { Loading } from '../Loading'
 import * as S from './styles'
 
-export const Footer = () => {
+interface FooterInterface {
+  loadingGlobal: boolean
+}
+
+export const Footer: React.FC<FooterInterface> = ({ loadingGlobal }) => {
   const [email, setEmail] = useState('')
+  const [text, setText] = useState('Inscrever-se')
   const [loading, setLoading] = useState(false)
+  const [suscribed, setSuscribed] = useState(false)
 
   const onClickSubscribe = async () => {
-    setLoading(true)
-    await newSubscription(email)
+    try {
+      if (loading || suscribed) return
 
-    localStorage.setItem('already_subscribed', 'true')
+      setText('Carregando...')
 
-    setLoading(false)
+      setLoading(true)
+      await newSubscription(email)
+
+      setLoading(false)
+      setSuscribed(true)
+      setText('Inscrito!')
+    } catch {
+      setLoading(false)
+      setText('Tente novamente!')
+    }
   }
 
   return (
     <>
-      {loading && <Loading />}
-      <S.Container>
-        <S.Title>Deseja receber notificação assim que um novo post for disponibilizado?</S.Title>
-        <S.Content>
-          <S.Input onChange={(e) => setEmail(e.target.value)} placeholder="Escreva seu e-mail aqui" type="email" />
-          <S.Button onClick={onClickSubscribe}>Inscrever-se</S.Button>
-        </S.Content>
-      </S.Container>
+      {!loadingGlobal && (
+        <>
+          {loading && <Loading />}
+          <S.Container>
+            <S.Title>Deseja receber notificação assim que um novo post for disponibilizado?</S.Title>
+            <S.Content>
+              <S.Input onChange={(e) => setEmail(e.target.value)} placeholder="Escreva seu e-mail aqui" type="email" />
+              <S.Button loading={loading || suscribed} onClick={onClickSubscribe}>
+                {text}
+              </S.Button>
+            </S.Content>
+          </S.Container>
+        </>
+      )}
     </>
   )
 }
--- src/pages/blog-page/index.tsx
+++ src/pages/blog-page/index.tsx
@@ -73,7 +73,7 @@ const BlogPage = () => {
             {post?.markdown}
           </ReactMarkdown>
         </S.ContainerMarkdown>
-        <Footer />
+        <Footer loadingGlobal={loading} />
       </Container>
     </>
   )
