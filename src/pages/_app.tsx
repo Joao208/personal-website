a60697eab30171761036dbfc5cc8cb890f0bf056
João Augusto <joao.augusto@foodzilla.com.br>
2022-01-08 20:16:26 -0300

    feat: adding fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { createGlobalStyle } from 'styled-components'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -33,6 +34,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <GlobalStyle />
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="theme-color" content="#1C2A34" media="(prefers-color-scheme: dark)" />
+        <link rel="manifest" href="manifest.json" />
+
         <meta name="keywords" content="Full Stack, Dev, Programador, Backend, Web, Frontend" />
         <meta name="author" content="João Augusto (joao208)" />
       </Head>
