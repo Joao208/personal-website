d51acc8a30dd9565e254053da2d8fcb361e95639
João Augusto <joao.augusto@foodzilla.com.br>
2022-01-14 18:01:14 -0300

    feat: small fix

--- src/components/DefaultHead/renderHead.tsx
+++ src/components/DefaultHead/renderHead.tsx
@@ -16,20 +16,19 @@ export const RenderHead: React.FC<RenderHeadInterface> = ({
   return (
     <>
       <meta property="og:title" content={titleComplete} />
-      <meta property="og:image" content={image} />
+      <meta name="image" property="og:image" content={image} />
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={site_name} />
       <meta property="og:type" content={type} />
       <meta property="og:image:width" content="1484" />
       <meta property="og:image:height" content="1200" />
       <meta prefix="og: http://ogp.me/ns#" property="og:title" content={titleComplete} />
-      <meta prefix="og: http://ogp.me/ns#" property="og:image" content={image} />
+      <meta name="image" prefix="og: http://ogp.me/ns#" property="og:image" content={image} />
       <meta prefix="og: http://ogp.me/ns#" property="og:description" content={description} />
       <meta prefix="og: http://ogp.me/ns#" property="og:site_name" content={site_name} />
       <meta prefix="og: http://ogp.me/ns#" property="og:type" content={type} />
       <meta prefix="og: http://ogp.me/ns#" property="og:image:width" content="1484" />
       <meta prefix="og: http://ogp.me/ns#" property="og:image:height" content="1200" />
-      <meta prefix="og: http://ogp.me/ns#" property="og:url" content="https://joaobarros.blog" />
       <meta prefix="og: http://ogp.me/ns#" name="twitter:card" content="summary_large_image" />
     </>
   )
